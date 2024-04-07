08fd72a58c1572c76ea37e63c806a52ce001d8ba
Mriganka Brahma <mrigankabrahma222@gmail.com>
2024-04-07 21:24:58 +0530

    change

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function displayItems(){
 
         var itemPrice= document.createElement('p');
         itemPrice.setAttribute('id','item-price');
-        itemPrice.innerText= 'Price : $ ' + item.price;
+        itemPrice.innerText= 'Price : ₹' + item.price;
 
         itemCard.appendChild(cardTop);
         itemCard.appendChild(img);
@@ -87,7 +87,7 @@ function displayItems(){
 
         var itemPrice= document.createElement('p');
         itemPrice.setAttribute('id','item-price');
-        itemPrice.innerText= 'Price : $ ' + item.price;
+        itemPrice.innerText= 'Price : ₹ ' + item.price;
 
         itemCard.appendChild(cardTop);
         itemCard.appendChild(img);
